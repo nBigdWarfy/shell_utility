fdca8bacc288a442dddc4768b57f36f9968d5a22
nBigdWarfy <105408600+nBigdWarfy@users.noreply.github.com>
2024-03-25 00:37:02 -0300

    Delete prev.ps1

--- prev.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host "Downloading and installing files..."
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/nBigdWarfy/shell_utility/main/shellpanel.ps1 -OutFile Downloads
-Start-Process Downloads/shellpanel.ps1
